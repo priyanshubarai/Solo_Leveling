df83ee63e2b55c7f0895973d68e6d2acfaacc233
priyanshubarai <priyanshubarai45@gmail.com>
2026-02-04 13:59:05 +0530

    clerk setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
 import Image from "next/image";
+import {
+  SignIn,
+  UserButton,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>HOME</div>
+    <div>
+      <div>HOME</div>
+      <div className="flex flex-col gap-2">
+        <SignedOut>
+          <SignInButton/>
+          <SignUpButton/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+          <SignOutButton/>
+        </SignedIn>
+      </div>
+    </div>
   );
 }
