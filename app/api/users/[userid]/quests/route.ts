993aaaaa912b7bde239c95bdf23d487f085f48f7
priyanshubarai <priyanshubarai45@gmail.com>
2026-02-07 18:59:31 +0530

    working on questsTAble

--- app/api/users/[userid]/quests/route.ts
+++ app/api/users/[userid]/quests/route.ts
@@ -1,3 +1,26 @@
-export async function POST(){
-    
+import { questsTable } from "@/db/schema";
+import db from "@/index";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: Promise<{ userid: string }> },
+) {
+  const { userid } = await params;
+  const { questtitle, questdesc } = await req.json();
+
+  const newQuest = await db
+    .insert(questsTable)
+    .values({
+      clerkuserid: userid,
+      questtitle: questtitle,
+      questdesc: questdesc,
+    })
+    .returning();
+
+  console.log("quest inserted : ", newQuest);
+  return NextResponse.json(
+    { message: "quest inserted", data: newQuest },
+    { status: 201 },
+  );
 }
