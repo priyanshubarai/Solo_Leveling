f5c0764ad1d268fabb3df1adbd44e40a02aee1ed
priyanshubarai <priyanshubarai45@gmail.com>
2026-02-07 15:16:49 +0530

    user insertion deletion

---
+++ app/api/users/route.ts
@@ -0,0 +1,70 @@
+import db from "@/index";
+import { usersTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+type NewUser = typeof usersTable.$inferInsert;
+
+export async function POST(req: NextRequest) {
+  const { clerkuserid, username, email } = await req.json();
+  const newUser = await db
+    .insert(usersTable)
+    .values({
+      clerkuserid: clerkuserid,
+      username: username,
+      email: email,
+    })
+    .returning();
+  console.log("User Created : ", newUser);
+  return NextResponse.json(
+    {
+      message: "User Created",
+      user: newUser,
+    },
+    { status: 201 },
+  );
+}
+
+export async function DELETE(req: NextRequest) {
+  const { clerkuserid } = await req.json();
+  const deletedUser = await db
+    .delete(usersTable)
+    .where(eq(usersTable.clerkuserid, clerkuserid))
+    .returning();
+  console.log("User Deleted : ", deletedUser);
+  return NextResponse.json(
+    { message: "User Deleted", user: deletedUser },
+    { status: 200 },
+  );
+}
+
+// async function main() {
+//   const user: typeof usersTable.$inferInsert = {
+//     name: 'John',
+//     age: 30,
+//     email: 'john@example.com',
+//   };
+//   await db.insert(usersTable).values(user);
+//   console.log('New user created!')
+//   const users = await db.select().from(usersTable);
+//   console.log('Getting all users from the database: ', users)
+//   /*
+//   const users: {
+//     id: number;
+//     name: string;
+//     age: number;
+//     email: string;
+//   }[]
+//   */
+//   await db
+//     .update(usersTable)
+//     .set({
+//       age: 31,
+//     })
+//     .where(eq(usersTable.email, user.email));
+//   console.log('User info updated!')
+//   await db.delete(usersTable).where(eq(usersTable.email, user.email));
+//   console.log('User deleted!')
+// }
+
+// main();
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+
+    
+
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+export default page
